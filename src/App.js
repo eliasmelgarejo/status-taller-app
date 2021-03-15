69173ac96ea4c19d35155b9ff0ae336ec12676b0
Elias Melgarejo <eliasmelgarejo@gmail.com>
2021-03-15 17:33:59 -0300

    Agregue opción de navegar con botones a diferentes vistas - Dashboard y Ststistics

--- src/App.js
+++ src/App.js
@@ -1,46 +1,112 @@
+import React, { Component } from "react";
 //import { DiasHabilesComponent } from './components/DiasHabiles/DiasHabilesComponent';
 import Dashboard from './components/Dashboard/index';
-//import TitleComponent from './components/Dashboard/DashboardHeader/TitleComponent';
-//import logo from './logo.svg';
+import Statistics from './components/Statistics/index';
 import './App.css';
+import iconDashbord from './components/Statistics/assets/dashboard_32.png';
+import iconStatistics from './components/Statistics/assets/combo-chart-32.png';
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header mytitle={'STATUS TALLER'}></Header>
+//       <Main></Main>
+//       <Footer></Footer>
+//     </div>
+//   );
+// }
 
-function Header({ mytitle }) {
-  return (
-    <header className="titleComponent" id="Header">
-      <div><h2>{mytitle}</h2></div>
-    </header>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      _viewDashboard: true,
+      _hasLogin: false,
+    }
 
-function Main() {
-  return (
-    <main className=" flex justify-around" id="Main">
-      <Dashboard></Dashboard>
-      <p></p>
-    </main>
-  );
-}
+    this.handleSignIn = this.handleSignIn.bind(this);
+    this.handleSignOut = this.handleSignOut.bind(this);
 
-function Footer() {
-  return (
-    <footer
-      className="titleComponent p-2 text-xs flex justify-around items-center bg-gray-900 text-white"
-      id="Footer"
-    >
-      <div>© 2021 Departamento de TI</div>
-    </footer>
-  );
-}
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <Header mytitle={'STATUS TALLER'}></Header>
-      <Main></Main>
-      <Footer></Footer>
-    </div>
-  );
+  handleSignIn = () => {
+    this.setState({ _hasLogin: true });
+  }
+
+  handleSignOut = () => {
+    this.setState({ _hasLogin: false });
+  }
+
+  handleDashboardButtom = () => {
+    console.log('handleDashboardButtom!!');
+    this.setState({ _viewDashboard: true });
+  }
+
+  handleStatisticsButtom = () => {
+    console.log('handleStatisticsButtom!!');
+    this.setState({ _viewDashboard: false });
+  }
+
+  componentDidMount() {
+    this.setState({ _viewDashboard: true });
+  }
+
+  render() {
+
+    const Header = ({ mytitle }) => {
+      return (
+        <header className="titleComponent" id="Header">
+          <div>
+            <h2>{mytitle}</h2>
+            <div className="row">
+              <span>--</span>
+              <button onClick={e => this.handleDashboardButtom(e)}><img src={iconDashbord}></img></button>
+              <button onClick={e => this.handleStatisticsButtom(e)}><img src={iconStatistics}></img></button>
+            </div>
+          </div>
+        </header>
+      );
+    }
+
+    const Main = ({ viewDashboard }) => {
+      console.log("**** viewDashboard: ", viewDashboard);
+      if (viewDashboard) {
+        return (
+          <main className="flex justify-around" id="Main">
+            <Dashboard></Dashboard>
+            <p></p>
+          </main>
+        );
+      } else {
+        return (
+          <main className="flex justify-around" id="Main">
+            <Statistics></Statistics>
+            <p></p>
+          </main>
+        );
+      }
+
+    }
+
+    const Footer = () => {
+      return (
+        <footer
+          className="titleComponent p-2 text-xs flex justify-around items-center bg-gray-900 text-white"
+          id="Footer"
+        >
+          <div>© 2021 Departamento de TI</div>
+        </footer>
+      );
+    }
+
+    return (
+      <div className="App">
+        <Header mytitle={'STATUS TALLER'}></Header>
+        <Main viewDashboard={this.state._viewDashboard}></Main>
+        <Footer></Footer>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Statistics/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Statistics extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>Grafico de estadisticas</div>
+         );
+    }
+}
+ 
+export default Statistics;
